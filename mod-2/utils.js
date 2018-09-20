1eb76515f81952a40eecbef718bd0543df2df7c0
Maurice de Beijer <maurice.de.beijer@gmail.com>
2018-09-20 19:55:13 +0200

    Added utils

---
+++ mod-2/utils.js
@@ -0,0 +1,15 @@
+export default class Demo {
+  constructor(name) {
+    this.name = name;
+  }
+
+  print() {
+    console.log(this.name);
+  }
+}
+
+export const y = 1;
+
+
+
+export var zzz = "zzz"
